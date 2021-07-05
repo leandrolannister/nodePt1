b322be8c8f31cd4555bf0bb76edbcfbecc740dc4
leandrolannister <leandrohendrix@gmail.com>
2021-07-05 19:48:50 -0300

    Module

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-const express = require('express')();
+const app = require('./config/express')();
 
-express.set('view engine', 'ejs');
-
-express.get('/produtos', (req, res) => { 
+app.get('/produtos', (req, res) => { 
    res.render("produtos/lista"); 
 });
 
-express.listen(3000, function(){
+app.listen(3000, function(){
     console.log('Server is running');
 });
 
---
+++ config/express.js
@@ -0,0 +1,9 @@
+const express = require('express')();
+express.set('view engine', 'ejs');
+
+module.exports = () => {
+    return express;
+};
+
+
+
