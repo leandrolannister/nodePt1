e1a8050a45d857c9be7a59049d39ce2f03a2cd77
leandrolannister <leandrohendrix@gmail.com>
2021-07-14 19:05:39 -0300

    Connection

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,16 +1,13 @@
+const connection = require('../app/infra/connectionFactory')();
+
 module.exports = (app) => {
     app.get('/livros', (req, res) => { 
-        let mysql = require('mysql');
-        let connection = mysql.createConnection({
-            host: '127.0.0.1',
-            user: 'root',
-            password: '',
-            database: 'livro'
-        });      
         let query = 'SELECT id, nome FROM  livros;';
 
         connection.query(query, (error, data) => {
             res.render('produtos/lista', {lista: data});
-        });         
+        }); 
+        
+        connection.end();
      });
 }
